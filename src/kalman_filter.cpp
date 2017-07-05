d4cdb43a78f57194881f60232add63361c9a6ddf
NG FANG KIANG <kiang.ng@hotmail.com>
2017-07-05 16:53:20 +0800

    updates

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include <math.h>       /* pow */
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -9,12 +10,13 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+  // Kalman Filter Variable
+  x_ = x_in;  // Object State
+  P_ = P_in;  // Object covariance Matrix
+  F_ = F_in;  // State Transition Matrix
+  H_ = H_in;  // Measurement Matrix
+  R_ = R_in;  // Measurement Covariance Matrix
+  Q_ = Q_in;  // Process Covariance Matrix
 }
 
 void KalmanFilter::Predict() {
@@ -22,6 +24,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,11 +35,64 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K =  PHt * Si;
+
+  //new state
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
+  */
+
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  
+
+  float rho = sqrt(px*px + py*py);
+  float theta = atan2(py, px);
+  float ro_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+  if (fabs(rho) < 0.001) {ro_dot = 0;}
+
+  VectorXd z_pred(3);
+  z_pred << rho, theta, ro_dot;
+
+  VectorXd y = z - z_pred;
+  
+  //angle normalization
+  while (y(1)> M_PI) y(1)-=2.*M_PI;
+  while (y(1)<-M_PI) y(1)+=2.*M_PI;
+  
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  // New State
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
+  /*
+  //KF Prediction step
+  x_ = F_ * x_ + u;
+  MatrixXd Ft = F.transpose();
+  P_ = F_ * P_ * Ft + Q_;
   */
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,39 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() != ground_truth.size()
+      || estimations.size() == 0){
+      std::cout << "Invalid estimation or ground_truth data" << std::endl;
+      return rmse;
+  }
+
+  //accumulate squared residuals
+  for (unsigned int i = 0; i < estimations.size(); ++i){
+
+      VectorXd residual = estimations[i] - ground_truth[i];
+
+      //coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse / estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
+
+	
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +55,30 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  
+  MatrixXd Hj(3, 4);
+
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // Avoid repeated calculation
+  float rho_2 = px * px + py * py;
+  float rho = sqrt(rho_2);
+  float rho_3 = (rho_2 * rho);
+  
+  //check division by zero
+  if(fabs(rho_2) < 0.001){
+    cout << "CalculateJacobian() Error - Division by Zero" << endl;
+    return Hj;
+  }
+
+  //compute the Jacobian matrix
+  Hj << (px/rho), (py/rho), 0, 0,
+        -(py/rho_2), (px/rho_2), 0, 0,
+        py*(vx*py - vy*px)/rho_3, px*(px*vy - py*vx)/rho_3, px/rho, py/rho;
+
+  return Hj;
 }
